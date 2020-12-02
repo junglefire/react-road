c821138fe1323d2aa88c031d9b577b09590ea77b
王征 <alex.wz@alibaba-inc.com>
2020-12-02 17:08:45 +0800

    add react-redux-real

---
+++ react-redux-real/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Header from './Header'
+import Content from './Content'
+
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+
+// 删除 Index 里面所有关于 context 的代码
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
+// 把 Provider 作为组件树的根节点
+ReactDOM.render(
+  <Provider store={store}>
+    <Index />
+  </Provider>,
+  document.getElementById('root')
+)
